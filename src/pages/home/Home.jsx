69bfb13c6a725d0a25bd14f2b82b8871e1135cbf
abhishekch33 <abhishek.2101134ec@iiitbh.ac.in>
2024-01-28 20:53:38 +0530

    error fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "./style.scss";
-import Hero from "./heroSection/hero";
+import Hero from "./heroSection/HeroSection";
 import Trending from "./trending/Trending";
 import Popular from "../popular/Popular";
 import TopRated from "./topRated/TopRated";
