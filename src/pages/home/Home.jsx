1c7d3d7ad979e9a9fdf78a0cd3a3097f77f12f32
abhishekch33 <abhishek.2101134ec@iiitbh.ac.in>
2024-01-28 20:50:52 +0530

    error fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   return (
     <div className="homePage">
       <React.StrictMode>
-        <Hero></Hero>
+        <Hero/>
         <Trending/>
         <Popular/>
         <TopRated/>
